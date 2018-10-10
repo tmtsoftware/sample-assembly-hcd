645efbbcbad268f03dbc89b74dde34b86f7a5a25
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-10-10 23:32:37 +0530

    Update latest csw sha and make it runnable

--- project/Libs.scala
+++ project/Libs.scala
@@ -14,12 +14,12 @@ object CSW {
   //for testing local changes use following
 //  private val Org     = "org.tmt"
 //  private val Version = "0.1-SNAPSHOT"
-  private val Org     = "com.github.tmtsoftware.csw-prod"
-  private val Version = "fd0a83a74"
+  private val Org     = "com.github.tmtsoftware.csw"
+  private val Version = "ed3094a13"
 
-  val `csw-location-client`      = Org %% "csw-location-client"      % Version
-  val `csw-config-client` = Org %% "csw-config-client" % Version
-  val `csw-logging`       = Org %% "csw-logging"       % Version
-  val `csw-framework`     = Org %% "csw-framework"     % Version
-  val `csw-command`       = Org %% "csw-command"       % Version
+  val `csw-location-client` = Org %% "csw-location-client" % Version
+  val `csw-config-client`   = Org %% "csw-config-client"   % Version
+  val `csw-logging`         = Org %% "csw-logging"         % Version
+  val `csw-framework`       = Org %% "csw-framework"       % Version
+  val `csw-command`         = Org %% "csw-command"         % Version
 }
